cfb9fc60d86ea8c5ac14c2915653143752bff35f
cleverson85 <cleverson85@gmail.com>
2021-08-07 23:58:47 -0400

    Capitulo 2: Desafio: Cart Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { ToastContainer } from 'react-toastify';
-
-import Routes from './routes';
-import GlobalStyles from './styles/global';
-import Header from './components/Header';
-import { CartProvider } from './hooks/useCart';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Header from "./components/Header";
+import { CartProvider } from "./hooks/useCart";
+import Routes from "./routes";
+import GlobalStyles from "./styles/global";
 
 const App = (): JSX.Element => {
-  return (
-    <BrowserRouter>
-      <CartProvider>
-        <GlobalStyles />
-        <Header />
-        <Routes />
-        <ToastContainer autoClose={3000} />
-      </CartProvider>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<CartProvider>
+				<GlobalStyles />
+				<Header />
+				<Routes />
+				<ToastContainer autoClose={3000} />
+			</CartProvider>
+		</BrowserRouter>
+	);
 };
 
 export default App;
